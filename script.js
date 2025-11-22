e1bbb79b987bda0ed6f142e7bec0bb4790e9af79
sajjad roohandeh <Roohandehsredi6@gmail.com>
2025-11-22 19:16:03 +0330

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+  const btn = document.getElementById("go-top");
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    btn.style.display = "flex";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+document.getElementById("go-top").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const menu = document.querySelector(".menu-items");
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".sajjad-nav");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu-items-display");
+  nav.classList.toggle("nav-after-menu");
+});
