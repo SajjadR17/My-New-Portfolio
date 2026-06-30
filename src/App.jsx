c63703326aa00540cdf30e4e042d0967dae80df5
Sajjad Roohandeh <Roohandehsredi6@gmail.com>
2026-06-30 23:44:57 +0330

    Update structure and ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import Aos from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
-import VantaBackground from "./components/VantaBackground";
 import Header from "./components/Header";
-import HiSection from "./components/HiSection";
-import SkillsSection from "./components/SkillsSection";
-import ProjectsSection from "./components/ProjectsSection";
-import AboutSection from "./components/AboutSection";
-import ContactSection from "./components/ContactSection";
-import Footer from "./components/Footer";
-import topIcon from "./assets/Top-Icon.webp";
 
 function App() {
-  useEffect(() => {
-    Aos.init({ once: true });
-  }, []);
-
   return (
     <>
-      <div id="go-top">
-        <img src={topIcon} alt="" />
-      </div>
-      <VantaBackground />
+      <div class="bg-grid"></div>
+      <div class="glow1"></div>
+      <div class="glow2"></div>
       <Header />
-      <main>
-        <HiSection />
-        <hr />
-        <AboutSection />
-        <hr />
-        <SkillsSection />
-        <hr />
-        <ProjectsSection />
-        <hr />
-        <ContactSection />
-      </main>
-      <Footer />
     </>
   );
 }
